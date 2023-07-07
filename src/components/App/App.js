079f8f2b2c62bfb92bd275bb688b86de738d0827
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-07 18:08:09 -0400

    Add loading and error states

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,11 +11,14 @@ function App() {
   const [allMovies, setAllMovies] = useState([]);
   const [singleMovie, setSingleMovie] = useState(null);
   const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     const apiCall = async () => {
       try {
         setAllMovies(await getAllMovies())
+        setLoading(false)
       } catch (error) {
         setError(error)
       }
@@ -34,6 +37,7 @@ function App() {
 
   return (
     <main className="main-app">
+      {error && <h1 style={{color: 'red'}}>Something went wrong, please try again</h1>}
       {singleMovie ? 
         <>
           <ExitMovie viewAll={viewAll} />
@@ -41,6 +45,7 @@ function App() {
         </> : 
         <>
           <img className='main-title' src={banner} alt='old school theater banner with the title rancid tomatillos' />
+          {loading && <h2 style={{color: 'red'}}>Loading...</h2>}
           <MoviesBox movies={allMovies} viewMovie={viewMovie}/>
         </>
       }

c6e563fbf5b9201340644feef515cdc683b1e5d0
Jason Alberto <alberto.jason@gmail.com>
2023-07-07 19:42:05 -0400

    Remove callback function handling the cover click and delete file

--- src/components/ExitMovie/ExitMovie.js
+++ src/components/ExitMovie/ExitMovie.js
@@ -1,8 +1,7 @@
-import './ExitMovie.css';
 import exitBanner from '../../images/old-school-return-banner.png'
 
 const ExitMovie = ({viewAll}) => {
-  return <img className='main-title' src={exitBanner} alt='old school theater banner with the title rancid tomatillos' onClick={() => viewAll()}/>
+  return <img className='main-title' src={exitBanner} alt='old school theater banner with the title rancid tomatillos' onClick={viewAll}/>
 };
 
 export default ExitMovie;

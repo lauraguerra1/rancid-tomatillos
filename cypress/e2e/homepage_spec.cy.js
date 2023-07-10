e4ddd855b5e0825eabced5b685fbc36a07a3a37b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-10 18:01:04 -0400

    Test 400 and 500 level homepage errors

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -36,11 +36,27 @@ describe('template spec', () => {
   })
 
   it('should display a banner and movie list', () => {
-    cy.get('.main-title')
+    cy.get('.title-container').find('.main-title')
       .get('.movie-container').find('.cover-container').should('have.length', 3)
       .get('.cover-container').first().find('.movie-cover[alt="Black Adam"]')
       .get('.cover-container').first().contains('p', '🍅 4.00')
       .get('.cover-container').last().find('.movie-cover[alt="R.I.P.D. 2: Rise of the Damned"]')
       .get('.cover-container').last().contains('p', '🍅 7.00')
+  })
+
+  it('should display a 500 level error', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 500})
+    .visit('http://localhost:3000')
+    .get('.title-container').find('.main-title')
+    .get('main').contains('h1', 'HTTP Error: 500 -- Please try again')
+  })
+  
+  it('should display a 400 level error', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 400})
+    .visit('http://localhost:3000')
+    .get('.title-container').find('.main-title')
+    .get('main').contains('h1', 'HTTP Error: 400 -- Please try again')
   })
 })

01e77e768ff39df7f0917291b36ae18b330f4473
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-13 09:27:16 -0400

    Stub trailer requests

--- cypress/e2e/singlemovie_spec.cy.js
+++ cypress/e2e/singlemovie_spec.cy.js
@@ -10,6 +10,156 @@ describe('single movie page', () => {
   let blackAdam;
   let riseOfDamned;
 
+  const blackAdamVideos = {
+    "videos": [
+    {
+    "id": 1,
+    "movie_id": 436270,
+    "key": "I9B6rwW35GQ",
+    "site": "YouTube",
+    "type": "Featurette"
+    },
+    {
+    "id": 2,
+    "movie_id": 436270,
+    "key": "b1pMQasDnhM",
+    "site": "YouTube",
+    "type": "Clip"
+    },
+    {
+    "id": 3,
+    "movie_id": 436270,
+    "key": "F_pYZ2HEW3E",
+    "site": "YouTube",
+    "type": "Featurette"
+    },
+    {
+    "id": 4,
+    "movie_id": 436270,
+    "key": "0RnGge4t_ZU",
+    "site": "YouTube",
+    "type": "Behind the Scenes"
+    },
+    {
+    "id": 5,
+    "movie_id": 436270,
+    "key": "wajuHqcA_Rg",
+    "site": "YouTube",
+    "type": "Featurette"
+    },
+    {
+    "id": 6,
+    "movie_id": 436270,
+    "key": "NtHe_10HpSk",
+    "site": "YouTube",
+    "type": "Featurette"
+    },
+    {
+    "id": 7,
+    "movie_id": 436270,
+    "key": "NnjCUbIfKoM",
+    "site": "YouTube",
+    "type": "Featurette"
+    },
+    {
+    "id": 8,
+    "movie_id": 436270,
+    "key": "L1D0I_Q93UY",
+    "site": "YouTube",
+    "type": "Featurette"
+    },
+    {
+    "id": 9,
+    "movie_id": 436270,
+    "key": "3-mscP3eIts",
+    "site": "YouTube",
+    "type": "Behind the Scenes"
+    },
+    {
+    "id": 10,
+    "movie_id": 436270,
+    "key": "TjhoHrYX4NY",
+    "site": "YouTube",
+    "type": "Featurette"
+    },
+    {
+    "id": 11,
+    "movie_id": 436270,
+    "key": "DoQBBTdCNQE",
+    "site": "YouTube",
+    "type": "Featurette"
+    },
+    {
+    "id": 12,
+    "movie_id": 436270,
+    "key": "PAe6E8msc_Y",
+    "site": "YouTube",
+    "type": "Teaser"
+    },
+    {
+    "id": 13,
+    "movie_id": 436270,
+    "key": "2RV5D0xK-9I",
+    "site": "YouTube",
+    "type": "Teaser"
+    },
+    {
+    "id": 14,
+    "movie_id": 436270,
+    "key": "YQweXN2fBz4",
+    "site": "YouTube",
+    "type": "Teaser"
+    },
+    {
+    "id": 15,
+    "movie_id": 436270,
+    "key": "mkomfZHG5q4",
+    "site": "YouTube",
+    "type": "Trailer"
+    },
+    {
+    "id": 16,
+    "movie_id": 436270,
+    "key": "DvCFc5JtkkA",
+    "site": "YouTube",
+    "type": "Trailer"
+    },
+    {
+    "id": 17,
+    "movie_id": 436270,
+    "key": "N73oTiIIJe0",
+    "site": "YouTube",
+    "type": "Teaser"
+    },
+    {
+    "id": 21,
+    "movie_id": 436270,
+    "key": "JaV7mmc9HGw",
+    "site": "YouTube",
+    "type": "Trailer"
+    }
+    ]
+    }
+
+  const riseOfDamnedVideos = {
+    "videos": [
+    {
+    "id": 37,
+    "movie_id": 1013860,
+    "key": "gtM9lv0clGc",
+    "site": "YouTube",
+    "type": "Clip"
+    },
+    {
+    "id": 39,
+    "movie_id": 1013860,
+    "key": "L_REOJnLLNI",
+    "site": "YouTube",
+    "type": "Trailer"
+    }
+    ]
+    }
+
   beforeEach(() => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
       statusCode: 200,
@@ -90,6 +240,7 @@ describe('single movie page', () => {
 
   it('should select a movie and see it\'s details and return home', () => {
     stubSingleFetch(436270, 200, blackAdam)
+    stubSingleFetch('436270/videos', 200, blackAdamVideos)
     cy.get('.cover-container').first().find('.movie-cover[alt="Black Adam"]').click()
       .url().should('eq', 'http://localhost:3000/436270')
       .get('.movie-detail-container').children().should('have.length', 2)
@@ -104,6 +255,7 @@ describe('single movie page', () => {
 
   it('should select a different movie and see it\'s details and return home', () => {
     stubSingleFetch(1013860, 200, riseOfDamned)
+    stubSingleFetch('1013860/videos', 200, riseOfDamnedVideos)
     cy.get('.cover-container').next().find('.movie-cover[alt="R.I.P.D. 2: Rise of the Damned"]').click()
       .url().should('eq', 'http://localhost:3000/1013860')
       .get('.movie-detail-container').children().should('have.length', 2)
@@ -118,6 +270,7 @@ describe('single movie page', () => {
 
   it('should display a 500 level error', () => {
     stubSingleFetch(436270, 500, blackAdam)
+    stubSingleFetch('436270/videos', 500, blackAdamVideos)
     cy.get('.cover-container').first().find('.movie-cover[alt="Black Adam"]').click()
       .url().should('eq', 'http://localhost:3000/436270')
       .get('.title-container').find('.main-title')
@@ -126,6 +279,7 @@ describe('single movie page', () => {
 
   it('should display a 404 error', () => {
     stubSingleFetch(436270, 404, blackAdam)
+    stubSingleFetch('436270/videos', 404, blackAdamVideos)
     cy.get('.cover-container').first().find('.movie-cover[alt="Black Adam"]').click()
       .url().should('eq', 'http://localhost:3000/436270')
       .get('.title-container').find('.main-title')
